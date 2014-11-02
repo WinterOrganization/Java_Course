9bddbb1311f64ca8b53ef6ef29fc6e4ec47fcf27
KynChX <1163197258@qq.com>
2014-11-02 20:48:16 +0800

    Revert "增加了shoot工程"
    This reverts commit 1ff835955b6aaa1685acef1a5cffec79d00ae365.

--- 001/HelloWorld.java
+++ 001/HelloWorld.java
@@ -1,7 +1,7 @@
-package cn.edu.cqu.www;
+package demo;
 public class HelloWorld{
 	public static void main(String args[])
 	{
-		System.out.printf("Hello World\n");
+		System.out.println("Hello World");
 	}
 }
--- 006/src/cqu/Test.java
+++ 006/src/cqu/Test.java
@@ -2,6 +2,8 @@ package cqu;
 
 import java.util.Scanner;
 
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 
 public class Test {
 
